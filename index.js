66eab233de80b102a7f25641a3dfdd1eead95bf4
Alexander Sennikov <gseno88@gmail.com>
2016-01-11 17:12:22 +0300

    Add simple deploy posting logic
    Signed-off-by: Alexander Sennikov <gseno88@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 /* jshint node: true */
 'use strict';
 
+var BasePlugin = require('ember-cli-deploy-plugin');
+var request = require('request-promise');
+
 module.exports = {
-  name: 'ember-cli-deploy-rollbar'
+  name: 'ember-cli-deploy-rollbar',
+
+  createDeployPlugin: function(options) {
+    var DeployPlugin = BasePlugin.extend({
+      name: options.name,
+
+      requiredConfig: ['accessToken'],
+
+      didDeploy: function(context) {
+        var token = this.readConfig('accessToken');
+        this.log('Creating new deploy in Rollbar');
+
+        return request({
+          uri: 'https://api.rollbar.com/api/1/deploy/',
+          method: 'POST',
+          body: {
+            access_token: token,
+            environment: context.config.build.environment,
+            revision: context.revisionData.revisionKey
+          },
+          resolveWithFullResponse: true
+        }).then(function() {
+          this.log('Deploy was successfully created in Rollbar');
+        });
+      }
+    });
+
+    return new DeployPlugin();
+  }
 };
